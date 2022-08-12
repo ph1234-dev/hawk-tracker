8f4c67e8bf73339fc0ce4264b458af5169e22ad4
ph1234-Hawk <35072905+ph1234-dev@users.noreply.github.com>
2022-08-12 21:54:18 +0800

    added date picker change event for weekly record

--- public/js/app.js
+++ public/js/app.js
@@ -18,61 +18,62 @@ $("#new-account-form").on("submit",function(e){
 
 var keyupTimeoutID = 0;
 
-$('#reg-username').on('input', function(e) {
-    clearTimeout(keyupTimeoutID);
-    let sourceTxt = $(this).val()
-    keyupTimeoutID = setTimeout(function() {
-        // perform the ajax call here
+// $('#reg-username').on('input', function(e) {
+//     clearTimeout(keyupTimeoutID);
+//     let sourceTxt = $(this).val()
+//     keyupTimeoutID = setTimeout(function() {
+//         // perform the ajax call here
 
-        $.ajax({
+//         $.ajax({
 
-            url : 'http://localhost:8000/check_username',
-            type : 'GET',
-            data : {
-                'username' : sourceTxt
-            },
-            // accept:'application/json',
-            contentType:'application/json',
-            dataType:'json',
-            success : function(data) {              
-                // alert('Data: '+ data,response);
-                console.log(`Username Check (success): ${data}`)
-                // check if username exists
-                // then toggle error
-                if (data==true){
-                    // alert("cannot")
-                    // remove class would show the text
-                    // adding it would hide it
-                    $("#reg-username-warning").removeClass("show-warning").text("Username already exists")
-                }else{
-                    // $("#reg-username-warning").removeClass("show-warning").text("")
-                    $("#reg-username-warning").addClass("show-warning").text("")
-                }
-            },
-            error : function(request,error)
-            {
-                // alert("Request: "+JSON.stringify(request));
-                console.log(`Username Check (fail): ${error}`)
-            }
-        });
+//             url : 'http://localhost:8000/check_username',
+//             type : 'GET',
+//             data : {
+//                 'username' : sourceTxt
+//             },
+//             // accept:'application/json',
+//             contentType:'application/json',
+//             dataType:'json',
+//             success : function(data) {              
+//                 // alert('Data: '+ data,response);
+//                 console.log(`Username Check (success): ${data}`)
+//                 // check if username exists
+//                 // then toggle error
+//                 if (data==true){
+//                     // alert("cannot")
+//                     // remove class would show the text
+//                     // adding it would hide it
+//                     $("#reg-username-warning").removeClass("show-warning").text("Username already exists")
+//                 }else{
+//                     // $("#reg-username-warning").removeClass("show-warning").text("")
+//                     $("#reg-username-warning").addClass("show-warning").text("")
+//                 }
+//             },
+//             error : function(request,error)
+//             {
+//                 // alert("Request: "+JSON.stringify(request));
+//                 console.log(`Username Check (fail): ${error}`)
+//             }
+//         });
 
-    }, 500);
-});
+//     }, 500);
+// });
 
-$('#show_record_today').on('click',function(){
-    alert("okay showing")
+$('#input-date-week-identifier').on('change',function(e){
+   // alert()
+
+    // ways to add value to routes
+    // Route::get('events/{event}/venues/{venue}', fn (Request $request, Event $event, Venue $venue) => /* ... */)->name('events.venues.show');
+    //  route('events.venues.show', [1, 2]);                 // 'https://ziggy.test/events/1/venues/2'
+    //  route('events.venues.show', { event: 1, venue: 2 }); // 'https://ziggy.test/events/1/venues/2'
+    let url = route('show.daily.specific.record',{
+        date: $(this).val()
+    });
+    // // url = url.replace(':id', id);
+
+    document.location.href=url;
 })
 
-// set date picker
-// document.getElementById("show_record_today").valueAsDate = new Date()
-
-// $(document).ready(function() {
-//     var date = new Date();
-//     var day = date.getDate();
-//     var month = date.getMonth() + 1;
-//     var year = date.getFullYear();
-//     if (month < 10) month = "0" + month;
-//     if (day < 10) day = "0" + day;
-//     var today = year + "-" + month + "-" + day +"T00:00";       
-//     $("#record_date_picker").attr("value", today);
-// });
+$('#landing-page-main-button').on('click',function(){
+    document.location.href=route('show.login.form')
+})
