baa57103c2982503990e7987c6d494c76b28ecc5
ph1234-Hawk <35072905+ph1234-dev@users.noreply.github.com>
2022-08-12 20:48:28 +0800

    removed test header. and main button as ndicated now redirect users to login page

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -1,11 +1,10 @@
 @extends('template')
 @section('content')
     <div class="text-giant-panel">
-        <h1>Updated test</h1>
         <span style="align-self: center;font-size: 10rem" class="icon-hawk text-giant"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span></span>
         <div class="text-giant-title">Change starts with you</div>
         <div class="text-giant-subtitle">Start doing things you can do today!</div>
-       <button class="text-giant-button button-primary">Start</button>
+        <button id="landing-page-main-button" class="text-giant-button button-primary">Start</button>
     </div>
     <div class="text-giant-panel text-giant-panel-plain">
         <div class="component-container">
